82e65913b18e0bd1060e21dc0f5aab15be9ed09b
Kenta Murata <mrkn@mrkn.jp>
2015-01-17 13:37:37 +0900

    Add Calculator class

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,7 +4,7 @@ describe Calculator do
   describe '#calculate' do
     context 'with 4*5+6&7|8' do
       it do
-        excpect(Calculator.new.calculate "4*5+6&7|8").to eq 44
+        expect(Calculator.new.calculate "4*5+6&7|8").to eq 44
       end
     end
   end
