350ff43bd19eacac66e849145c870cf1bf604b72
Kenta Murata <mrkn@mrkn.jp>
2015-01-17 15:37:10 +0900

    Use parser in calculate method

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,6 +1,22 @@
 class Calculator
   def calculate(expression)
-    44
+    evaluate(Parser.new(expression).parse)
+  end
+
+  def evaluate(sexp)
+    return sexp if Integer === sexp
+
+    op, x1, x2 = *sexp
+    case op
+    when :*
+      evaluate(x1) * evaluate(x2)
+    when :+
+      evaluate(x1) + evaluate(x2)
+    when :&
+      evaluate(x1) & evaluate(x2)
+    when :|
+      evaluate(x1) | evaluate(x2)
+    end
   end
 end
 
